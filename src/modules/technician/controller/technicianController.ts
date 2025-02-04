bc2fa48e204a909b794f149ed16c31de8aec21ad
Ireme-Nyawe <akimana.inono@gmail.com>
2025-02-04 16:54:25 +0200

    encrypt new technician password

--- src/modules/technician/controller/technicianController.ts
+++ src/modules/technician/controller/technicianController.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
 import technicianRepository from "../repository/technicianRepository";
+import { hashPassword } from "../../../helpers/auth";
 
 const addNewTechnician = async (req: Request, res: Response) => {
   try {
-    const technicianData = req.body;
+    let technicianData = req.body;
+    const hashedPassword = await hashPassword(technicianData.password)
+    technicianData = {...technicianData,password:hashPassword}
+
     const technician = await technicianRepository.createTechnician(technicianData)
 
     res.status(httpStatus.CREATED).json({
